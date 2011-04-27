beeb1a1a7da6a9bd579220b3a49865fa684c582d
Andrew Vasquez <andrew.vasquez@qlogic.com>
2011-04-27 13:30:10 -0700

    qla2xxx: Allow an override of the registered maximum LUN.
    The 'max_lun' value registered for each scsi_host is currently
    capped at 0xffff.  The new module parameter can allow for
    2nd-level flat-space addressing method-infrastructure to be
    supported.
    
    Signed-off-by: Andrew Vasquez <andrew.vasquez@qlogic.com>
    Signed-off-by: Madhuranath Iyengar <Madhu.Iyengar@qlogic.com>
    
    JIRA Key: RHEL6.0FC-86

--- drivers/scsi/qla2xxx/qla_gbl.h
+++ drivers/scsi/qla2xxx/qla_gbl.h
@@ -97,6 +97,7 @@ extern int ql2xetsenable;
 extern int ql2xshiftctondsd;
 extern int ql2xdbwr;
 extern int ql2xdontresethba;
+extern unsigned int ql2xmaxlun;
 extern int ql2xasynctmfenable;
 extern int ql2xenabledif;
 extern int ql2xenablehba_err_chk;
