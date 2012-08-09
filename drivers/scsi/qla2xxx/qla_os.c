fbb527bd898edb40eba58006df89fcb433fa4b17
Arun Easi <arun.easi@qlogic.com>
2012-08-09 12:01:17 -0700

    qla2xxx: Use the right field for container_of.
    JIRA Key: V2632FC-259
    
    Acked-by: Giridhar Malavali <giridhar.malavali@qlogic.com>
    Acked-by: Chad Dupuis <chad.dupuis@qlogic.com>

--- drivers/scsi/qla2xxx/qla_os.c
+++ drivers/scsi/qla2xxx/qla_os.c
@@ -3763,7 +3763,7 @@ void
 qla83xx_nic_core_unrecoverable_work(struct work_struct *work)
 {
 	struct qla_hw_data *ha =
-		container_of(work, struct qla_hw_data, nic_core_reset);
+		container_of(work, struct qla_hw_data, nic_core_unrecoverable);
 	scsi_qla_host_t *base_vha = pci_get_drvdata(ha->pdev);
 	uint32_t dev_state = 0;
 
@@ -3785,7 +3785,7 @@ void
 qla83xx_idc_state_handler_work(struct work_struct *work)
 {
 	struct qla_hw_data *ha =
-		container_of(work, struct qla_hw_data, nic_core_reset);
+		container_of(work, struct qla_hw_data, idc_state_handler);
 	scsi_qla_host_t *base_vha = pci_get_drvdata(ha->pdev);
 	uint32_t dev_state = 0;
 
