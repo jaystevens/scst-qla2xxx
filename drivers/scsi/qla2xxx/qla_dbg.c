7aeb5d44196ff4108a0a439d1716e822cd2a5405
Joe Carnuccio <joe.carnuccio@qlogic.com>
2013-08-07 13:44:39 -0700

    qla2xxx: Correctly print out/in mailbox registers.
    At mailbox/buffer debug level, print the correct values of the
    outgoing and incoming mailbox registers.
    
    JIRA Key: V2632FC-439

--- drivers/scsi/qla2xxx/qla_dbg.c
+++ drivers/scsi/qla2xxx/qla_dbg.c
@@ -15,8 +15,9 @@
  * |                              |                    | 0x141,0x144,   |
  * |                              |                    | 0x146          |
  * | Mailbox commands             |       0x117f       | 0x1018-0x1019, |
- * |                              |                    | 0x10ca,0x111a, |
- * |                              |                    | 0x111b         |
+ * |                              |                    | 0x10ca         |
+ * |                              |                    | 0x1115-0x1116  |
+ * |                              |                    | 0x111a-0x111b  |
  * | Device Discovery             |       0x2098       | 0x2020-0x2022, |
  * |                              |                    | 0x2016         |
  * | Queue Command and IO tracing |       0x3051       | 0x3006-0x300b  |
