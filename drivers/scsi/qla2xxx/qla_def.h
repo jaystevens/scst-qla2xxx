e858cc0341d1bb3097d6f1c77478c55a69293802
Chad Dupuis <chad.dupuis@qlogic.com>
2013-09-18 13:02:42 -0700

    qla2xxx: Add BSG interface for read/write serdes register.
    JIRA Key: V2632FC-462
    
    Acked-by: Chad Dupuis <chad.dupuis@qlogic.com>
    Acked-by: Harish Zunjarrao <harish.zunjarrao@qlogic.com>

--- drivers/scsi/qla2xxx/qla_def.h
+++ drivers/scsi/qla2xxx/qla_def.h
@@ -861,7 +861,6 @@ typedef struct {
  */
 #define MBC_LOAD_RAM			1	/* Load RAM. */
 #define MBC_EXECUTE_FIRMWARE		2	/* Execute firmware. */
-#define MBC_WRITE_RAM_WORD		4	/* Write RAM word. */
 #define MBC_READ_RAM_WORD		5	/* Read RAM word. */
 #define MBC_MAILBOX_REGISTER_TEST	6	/* Wrap incoming mailboxes */
 #define MBC_VERIFY_CHECKSUM		7	/* Verify checksum. */
@@ -936,6 +935,8 @@ typedef struct {
 /*
  * ISP24xx mailbox commands
  */
+#define MBC_WRITE_SERDES		0x3	/* Write serdes word. */
+#define MBC_READ_SERDES			0x4	/* Read serdes word. */
 #define MBC_SERDES_PARAMS		0x10	/* Serdes Tx Parameters. */
 #define MBC_GET_IOCB_STATUS		0x12	/* Get IOCB status command. */
 #define MBC_PORT_PARAMS			0x1A	/* Port iDMA Parameters. */
