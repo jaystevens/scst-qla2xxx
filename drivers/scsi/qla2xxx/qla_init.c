1aac2419f88a1c052b231a8b82df6a7a65896b5b
Joe Carnuccio <joe.carnuccio@qlogic.com>
2013-11-05 11:37:13 -0800

    qla2xxx: Enable the Flash Access Control (FAC) mailbox command.
    Allow 83xx/26xx to use the FAC MBC.
    
    JIRA Key: V2632FC-477
    
    Acked-by: Chad Dupuis <chad.dupuis@qlogic.com>
    Acked-by: Joe Carnuccio <joe.carnuccio@qlogic.com>

--- drivers/scsi/qla2xxx/qla_init.c
+++ drivers/scsi/qla2xxx/qla_init.c
@@ -1724,9 +1724,6 @@ enable_82xx_npiv:
 		}
 	}
 
-	if (IS_QLA83XX(ha))
-		goto skip_fac_check;
-
 	if (rval == QLA_SUCCESS && IS_FAC_REQUIRED(ha)) {
 		uint32_t size;
 
@@ -1739,7 +1736,7 @@ enable_82xx_npiv:
 			    "Unsupported FAC firmware (%d.%02d.%02d).\n",
 			    ha->fw_major_version, ha->fw_minor_version,
 			    ha->fw_subminor_version);
-skip_fac_check:
+
 			if (IS_QLA83XX(ha)) {
 				ha->flags.fac_supported = 0;
 				rval = QLA_SUCCESS;
