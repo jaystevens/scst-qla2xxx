35c9790c0d65cd2a66ddbb2183137893d424c5f6
Joe Carnuccio <joe.carnuccio@qlogic.com>
2014-02-21 21:09:47 -0800

    qla2xxx: ISP27xx queue index shadow registers.
    For ISP27xx use the request/response queue index shadow registers
    to avoid directly Pccess them on the PCI bus.
    
    Note: response queue index register is not used due to read-ahead algorithm.
    
    JIRA Key: V2632FC-557

--- drivers/scsi/qla2xxx/qla_def.h
+++ drivers/scsi/qla2xxx/qla_def.h
@@ -2667,6 +2667,7 @@ struct rsp_que {
 	uint32_t __iomem *rsp_q_out;
 	uint16_t  ring_index;
 	uint16_t  out_ptr;
+	uint16_t  *in_ptr;		/* queue shadow in index */
 	uint16_t  length;
 	uint16_t  options;
 	uint16_t  rid;
@@ -2693,6 +2694,7 @@ struct req_que {
 	uint32_t __iomem *req_q_out;
 	uint16_t  ring_index;
 	uint16_t  in_ptr;
+	uint16_t  *out_ptr;		/* queue shadow out index */
 	uint16_t  cnt;
 	uint16_t  length;
 	uint16_t  options;
@@ -2959,6 +2961,7 @@ struct qla_hw_data {
 #define IS_PI_SPLIT_DET_CAPABLE_HBA(ha)	(IS_QLA83XX(ha))
 #define IS_PI_SPLIT_DET_CAPABLE(ha)	(IS_PI_SPLIT_DET_CAPABLE_HBA(ha) && \
     (((ha)->fw_attributes_h << 16 | (ha)->fw_attributes) & BIT_22))
+#define IS_SHADOW_REG_CAPABLE(ha)	(IS_QLA27XX(ha))
 
 	/* HBA serial number */
 	uint8_t		serial0;
