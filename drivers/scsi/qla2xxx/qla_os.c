7674e00c6594fb002a094173756dbc9f42ab603c
Arun Easi <arun.easi@qlogic.com>
2014-07-09 10:46:18 -0700

    qla2xxx: Unload of qla2xxx driver crashes the machine
    Unload of qla2xxx driver crashes the machine with DEBUG_PAGEALLOC.
    
    JIRA Key: V2632FC-610
    
    Acked-by: Joe Carnuccio <joe.carnuccio@qlogic.com>
    Acked-by: Himanshu Madani <himanshu.madani@qlogic.com>

--- drivers/scsi/qla2xxx/qla_os.c
+++ drivers/scsi/qla2xxx/qla_os.c
@@ -3185,9 +3185,9 @@ qla2x00_remove_one(struct pci_dev *pdev)
 
 	qla2x00_free_device(base_vha);
 
-	scsi_host_put(base_vha->host);
-
 	qla2x00_clear_drv_active(base_vha);
+
+	scsi_host_put(base_vha->host);
 
 	qla2x00_unmap_iobases(ha);
 
